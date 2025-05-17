5a432d02abb1e86e6f3dc0fca467a9bd26b8c2c2
Jakub Dąbrowski <60621737+tuniemanicku@users.noreply.github.com>
2025-05-17 15:24:02 +0200

    Added comments for API usage and convention

--- AStar.py
+++ AStar.py
@@ -1,9 +1,15 @@
 from Algorithm import Algorithm
 
+#należy napisać algorytm by był przystosowany do wywołania konstruktora i nextStep
+#klasa window ma pola:  labyrinth.board[y][x], gdzie 1 - ściana, 0 - pole
+#                       labyrinth.exit, które ma pola exit.x i exit.y
+#                       exit defaultowo znajduje się w prawym-górnym rogu czyli (LABYRINTH_SIZE-1, 0) LABYRINTH_SIZE znajduje się w Labyrinth.py
+#                       start defaultowo uznaje się za (0, LABYRINTH_SIZE-1)
+#                       labirynt wyświetalny w oknie jest wczytywany zawsze z "save.txt" można to sparametryzować później
 class AStar(Algorithm):
     def __init__(self, window):
         super().__init__(window)
         print("Hello from A* file")
     def nextStep(self):
-        self.window.updateBoard(1,2)
+        self.window.updateBoard(x=1,y=2) #w ten sposób kolorujemy kratki (0,0) jest w lewym-górnym rogu
         self.window.updateLogs1("Hello from A* file")
--- AntAlgorithm.py
+++ AntAlgorithm.py
@@ -1,9 +1,16 @@
 from Algorithm import Algorithm
 
+#należy napisać algorytm by był przystosowany do wywołania konstruktora i nextStep
+#klasa window ma pola:  labyrinth.board[y][x], gdzie 1 - ściana, 0 - pole
+#                       labyrinth.exit, które ma pola exit.x i exit.y
+#                       exit defaultowo znajduje się w prawym-górnym rogu czyli (LABYRINTH_SIZE-1, 0) LABYRINTH_SIZE znajduje się w Labyrinth.py
+#                       start defaultowo uznaje się za (0, LABYRINTH_SIZE-1)
+#                       labirynt wyświetalny w oknie jest wczytywany zawsze z "save.txt" można to sparametryzować później
+
 class AntAlgorithm(Algorithm):
     def __init__(self, window):
         super().__init__(window)
         print("Hello from Ant algorithm file")
     def nextStep(self):
-        self.window.updateBoard(1,1)
-        self.window.updateLogs1("Hello from Ant algorithm file")
+        self.window.updateBoard(x=1,y=1) #w ten sposób kolorujemy kratki (0,0) jest w lewym-górnym rogu
+        self.window.updateLogs1("Hello from Ant algorithm file") #tak dodajemy wpis do logów
